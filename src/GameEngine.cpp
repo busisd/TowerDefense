c545b44706f73ccbe3c04783c6bab594a703ccbf
Daniel Busis <busisd@carleton.edu>
2021-07-02 02:01:50 -0400

    Made a general game engine

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,17 +1,52 @@
+#include <SDL2/SDL.h>
+
 #include <chrono>
 #include <iostream>
+#include <vector>
+
+class GameInputs {
+  void initialize() {
+  }
+
+  void getInputs(std::vector<int> &inputs) {
+  }
+
+  void cleanup() {
+  }
+};
+
+class GameSimulation {
+  void initialize() {
+  }
+
+  void update() {
+  }
+
+  void cleanup() {
+  }
+};
+
+class GameRenderer {
+  void initialize() {
+  }
+
+  void render(GameSimulation &game) {
+  }
+
+  void cleanup() {
+  }
+};
 
 const int FRAME_TIME_MS = 16;
 
 int main(int argv, char **args) {
-  std::cout << "TEST" << std::endl;
-
   bool running = true;
 
   // GameSimulation mySimulation;
   // GameRenderer myRenderer;
 
   // mySimulation.initialize();
+  // myRenderer.initialize();
 
   auto gameClock = std::chrono::high_resolution_clock();
   auto realTime = gameClock.now();
